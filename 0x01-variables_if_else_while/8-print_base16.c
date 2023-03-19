739f5f3b9eabf41803c35b4882ebe036ec08eeae
Mariam6652 <elachourimariam33@gmail.com>
2023-03-19 09:30:14 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	for (c = 'a' ; c <= 'f' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
