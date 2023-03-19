30f05df518e172ab0dedde4e2233db7e5b1e2bdb
Mariam6652 <elachourimariam33@gmail.com>
2023-03-19 08:09:47 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int dg;
+
+        for (dg = '0' ; dg < '10' ; dg++)
+        {
+                putchar(dg);
+        }
+                putchar('\n');
+        return (0);
+}
