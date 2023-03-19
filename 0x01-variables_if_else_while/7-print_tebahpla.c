d181b16fa9065666db893acd845b08e394a6f44c
Mariam6652 <elachourimariam33@gmail.com>
2023-03-19 08:56:15 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c
+
+	for (c = 'z' ; c >= 'z' ; c--)
+	{
+		putchar(c);
+	}
+	putchart('\n');
+	return (0);
+}
