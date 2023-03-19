469e1ef6c71280e022ab1eca6c7847d210cb536e
Mariam6652 <elachourimariam33@gmail.com>
2023-03-19 07:47:29 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dg;
+
+	for (dg = 0 ; dg < 10 ; dg++)
+		printf("%d", dg);
+		printf("\n");
+	return (0);
+}
