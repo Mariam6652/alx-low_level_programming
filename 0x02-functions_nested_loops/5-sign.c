59cfea6762e28ec3118dd9c4a93ffc8451865271
Mariam6652 <elachourimariam33@gmail.com>
2023-03-22 19:54:13 -0700

    task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *  @n: number tested
+ *  Return: 1 n greater than zero or 0 n zero or -1 n less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
