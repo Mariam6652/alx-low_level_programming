9033df2d5453fabf61a0cb8ea035254482a15a60
Mariam6652 <elachourimariam33@gmail.com>
2023-03-23 17:33:10 -0700

    tasl

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: variable 1 multiple
+ * @b: variable 1 multiple
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
