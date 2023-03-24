2db92f9119278330abde73c90a50e8015de2a651
Mariam6652 <elachourimariam33@gmail.com>
2023-03-23 19:49:45 -0700

    task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if ((i % 5 != 0) && (i % 3 == 0))
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
