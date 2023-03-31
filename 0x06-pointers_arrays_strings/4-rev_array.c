d36227259a0f008c3ab66589906257ca9ad05992
Mariam6652 <elachourimariam33@gmail.com>
2023-03-30 19:50:03 -0700

    task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: int
+ * @n: int
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, rev;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		rev = a[i];
+		a[i] = a[n];
+		a[n] = rev;
+	}
+}
