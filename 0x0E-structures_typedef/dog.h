7dc883e1a0385401a55e81e6b43bfb803e299a8d
Mariam6652 <myam6652@gmail.com>
2023-06-20 10:25:28 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog informations
+ * @name: char name of dog
+ * @age: float age of dog
+ * @owner: char owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
