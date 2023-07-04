e16ca84119f9b7336999fd9b06d12eec19636193
Mariam6652 <elachourimariam333@gmail.com>
2023-07-04 16:10:41 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include"lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked
+ * @h: const listint_t
+ * Return: size_t
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
